84cbe24de73b511780b5c55517ca43b58049320a
nyapblesso-a11y <nyapblesso@gmail.com>
2026-04-16 12:27:26 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+import createError  from'http-errors';
+import express  from 'express' ;
+import path  from 'path';
+import cookieParser  from 'cookie-parser';
+import logger  from 'morgan';
+
+import indexRouter from'./routes/index.js';
+import usersRouter from './routes/users.js';
+
+var app = express();
+
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  const statusCode = err.status || 500;
+  res.status(statusCode);
+  res.send({
+    error: {
+      status: statusCode,
+      message: err.message,
+      stack: req.app.get('env') === 'development' ? err.stack : {}
+    }
+  });
+});
+
+export default app;
