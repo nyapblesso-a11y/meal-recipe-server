cb4785524181309d0ba4c07415d5d9911054426f
nyapblesso-a11y <nyapblesso@gmail.com>
2026-04-16 15:27:59 +0100

    the controller for the recipe creaotion has been implemented

--- controller/controller.js
+++ controller/controller.js
@@ -10,3 +10,19 @@ export const getRecipes = async(req, res) => {
      res.status(500).json({error: "failed to fetch recipes"})
     }
 }
+
+//create recipe
+
+export const addRecipe = async(req, res) => {
+ try {
+    const recipe = await createRecipes(req.body)
+    res.status(201).json({
+        message: "Recipe sucessfully created!", recipe
+    })
+ }catch (err) {
+    res.status(500).json({ 
+      error: "Failed to create recipe",
+      message: err.message 
+    });
+  }
+}
