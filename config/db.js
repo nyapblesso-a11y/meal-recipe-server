a90fd7625f789919b907d0df1bca4ee6fcc3d3fe
nyapblesso-a11y <nyapblesso@gmail.com>
2026-04-16 12:46:39 +0100

    the database has beennsetupa and connect and now running on the server

---
+++ config/db.js
@@ -0,0 +1,26 @@
+import pkg from 'pg'
+import { env } from './env.js'
+
+const {Pool} = pkg 
+
+export const pool = new Pool ({
+ port: env.DB_PORT,
+ host: env.DB_HOST,
+ database: env.DB_NAME,
+ user: env.DB_USER,
+ password: env.DB_PASSWORD,
+})
+
+export const connectDB = async () => {
+     try {
+    const client = await pool.connect();
+    console.log('PostgreSQL connected:', env.DB_NAME);
+    client.release();
+  } catch (error) {
+    console.error(' PostgreSQL connection failed:', error);
+    process.exit(1);
+  }
+}
+
+
+ export default pool
---
+++ config/env.js
@@ -0,0 +1,12 @@
+import dotenv from 'dotenv'
+
+dotenv.config()
+
+export const env = {
+  PORT: process.env.PORT || 4040,
+  DB_HOST: process.env.DB_HOST,
+  DB_PORT: process.env.DB_PORT || 5432,
+  DB_NAME: process.env.DB_NAME,
+  DB_USER: process.env.DB_USER,
+  DB_PASSWORD: process.env.DB_PASSWORD,
+};
