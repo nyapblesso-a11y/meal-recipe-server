5fd91be29b40c814e4a2887cbec5f234432c6989
nyapblesso-a11y <nyapblesso@gmail.com>
2026-04-16 15:32:52 +0100

    the update controller has been implemented

--- controller/controller.js
+++ controller/controller.js
@@ -1,28 +1,53 @@
-import { createRecipes, updateRecipes, getAllRecipes, toggleFavorite, deleteRecipe } from "../model/recipeMoodel";
+import {
+  createRecipes,
+  updateRecipes,
+  getAllRecipes,
+  toggleFavorite,
+  deleteRecipe,
+} from "../model/recipeMoodel";
 
 // get all recipes
 
-export const getRecipes = async(req, res) => {
-    try {
-     const recipes = await getAllRecipes()
-     res.json(recipes)
-    } catch(err) {
-     res.status(500).json({error: "failed to fetch recipes"})
-    }
-}
+export const getRecipes = async (req, res) => {
+  try {
+    const recipes = await getAllRecipes();
+    res.json(recipes);
+  } catch (err) {
+    res.status(500).json({ error: "failed to fetch recipes" });
+  }
+};
 
 //create recipe
 
-export const addRecipe = async(req, res) => {
- try {
-    const recipe = await createRecipes(req.body)
+export const addRecipe = async (req, res) => {
+  try {
+    const recipe = await createRecipes(req.body);
     res.status(201).json({
-        message: "Recipe sucessfully created!", recipe
-    })
- }catch (err) {
-    res.status(500).json({ 
+      message: "Recipe sucessfully created!",
+      recipe,
+    });
+  } catch (err) {
+    res.status(500).json({
       error: "Failed to create recipe",
-      message: err.message 
+      message: err.message,
     });
   }
-}
+};
+
+// update recipe
+
+export const editRecipe = async (req, res) => {
+  try {
+    const update = await updateRecipes(req.param.id, req.body);
+    res.json({
+      message: "Recipe updated successfully",
+      update,
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({ error: "Failed to update recipe", message: err.message });
+  }
+};
+
+//
