dbdede063c6a42725a8992ecd7296b8dbf3d7cb6
nyapblesso-a11y <nyapblesso@gmail.com>
2026-05-04 13:05:12 +0100

    the database settings were adjsuted

--- config/db.js
+++ config/db.js
@@ -1,28 +1,27 @@
-import pkg from 'pg'
+import pkg from "pg";
 import dotenv from "dotenv";
+
 dotenv.config();
 
 const { Pool } = pkg;
 
-export const pool = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+
   ssl: {
     rejectUnauthorized: false,
   },
-  family: 4, // 🔥 FORCE IPv4 (this fixes your issue)
 });
 
-
 export const connectDB = async () => {
-     try {
+  try {
     const client = await pool.connect();
-    console.log("DB URL:", process.env.DATABASE_URL);
+    console.log("DB connected successfully");
     client.release();
   } catch (error) {
-    console.error(' PostgreSQL connection failed:', error);
+    console.error("PostgreSQL connection failed:", error);
     process.exit(1);
   }
-}
+};
 
-
- export default pool
+export default pool;
