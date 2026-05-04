e7cb62c997773d30d975237576b704d67a989c80
nyapblesso-a11y <nyapblesso@gmail.com>
2026-05-04 15:41:19 +0100

    the database settings were adjsuted

--- config/db.js
+++ config/db.js
@@ -1,6 +1,5 @@
 import pkg from "pg";
 import dotenv from "dotenv";
-
 dotenv.config();
 
 const { Pool } = pkg;
@@ -9,17 +8,19 @@ export const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
-  }
+  },
 });
 
 export const connectDB = async () => {
   try {
+    console.log("DB URL:", process.env.DATABASE_URL); // safe logging
+
     const client = await pool.connect();
-    console.log("DB connected");
     client.release();
+
+    console.log("✅ DB connected successfully");
   } catch (error) {
     console.error("DB ERROR:", error);
-   
+    process.exit(1);
   }
 };
-export default pool;
