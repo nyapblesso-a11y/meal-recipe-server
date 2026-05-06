8de633e8b4ee26580a1d980c967b300193f3aa86
nyapblesso-a11y <nyapblesso@gmail.com>
2026-05-06 10:43:22 +0100

    prefix fixed cors issue

--- app.js
+++ app.js
@@ -1,47 +1,57 @@
 import createError from "http-errors";
 import express from "express";
+import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import cors from "cors";
+import { fileURLToPath } from "url";
 
 import indexRouter from "./routes/index.js";
 import uploadRouter from "./routes/uploadRouter.js";
 import recipeRouter from "./routes/recipeRoutes.js";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 
 
-// app.use(
-//   cors({
-//     origin: [
-//       "http://localhost:5173",
-//       // "https://your-frontend-domain.vercel.app",
-//     ],
-//     credentials: true,
-//   })
-// );
+// MUST BE FIRST
+app.use(cors({
+  origin: "*",
+  methods: ["GET","POST","PUT","PATCH","DELETE","OPTIONS"],
+  allowedHeaders: ["Content-Type","Authorization"]
+}));
+
+
+// MUST HANDLE PREFLIGHT
+app.options("*", cors());
+
 
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-// ROUTES
+app.use("/uploads", express.static(
+  path.join(__dirname, "uploads")
+));
+
 app.use("/", indexRouter);
 app.use("/api/upload", uploadRouter);
 app.use("/api/recipes", recipeRouter);
 
-// 404 handler
-app.use((req, res, next) => {
+
+app.use((req,res,next)=>{
   next(createError(404));
 });
 
-app.use(function (err, req, res, next) {
-  console.log("🔥 GLOBAL ERROR:", err); // VERY IMPORTANT
+
+app.use((err,req,res,next)=>{
+  console.log("🔥 ERROR:", err);
 
   res.status(err.status || 500).json({
-    error: err.message,
-    stack: err.stack,
+    error: err.message
   });
 });
 
